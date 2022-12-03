9e8904ebff2889b73183d063b4ffe34e82cf5889
Dexks <117020774+Dexks@users.noreply.github.com>
2022-12-03 15:51:59 -0300

    Create server.py

---
+++ sockettcp/server.py
@@ -0,0 +1,62 @@
+from socket import *
+
+# ########## CHANGE HERE ########### #
+
+# IP and PORT for TCP listener
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 43210
+
+# if True, the server will keep listening for new connections after ending one
+PERSISTENT = False
+
+# ########## CHANGE HERE ########### #
+
+
+# Receive data, decode it and clears "\n"
+def recv(connected):
+    data = connected.recv(1024).decode().strip()
+    return data
+
+
+# Sends data in bytes
+def send(connected, data):
+    connected.send(data.encode())
+
+
+# Listens to connections
+def listen():
+    # Creates SOCKET TCP object
+    server = socket(AF_INET, SOCK_STREAM)
+
+    # Binds the socket "server" to the SERVER_IP and PORT
+    server.bind((SERVER_IP, SERVER_PORT))
+
+    # Listen to connections
+    server.listen(1)
+
+    datatosend = "Hello, client"
+
+    while True:
+        print("...")
+        # Await a connection
+        connected, client = server.accept()
+
+        # Prints the client tuple (ip, port)
+        print("Connection from -> ", client)
+
+        # Prints the received message
+        print("Data received:", recv(connected))
+
+        # Sends "hello" in bytes (encodes transforms 'str' in 'bytes')
+        send(connected, datatosend)
+        print("Data sent:", datatosend)
+
+        # Closes the connection
+        connected.close()
+
+        if not PERSISTENT:
+            break
+
+
+if __name__ == "__main__":
+    listen()
